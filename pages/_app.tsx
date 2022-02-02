b6ee96b6ddfd518cd2308f9fbc92cea24b0179b3
Matthew Marion Moncada <misakamikotocsr@gmail.com>
2022-02-01 19:08:23 -0700

    added google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,28 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Script from 'next/script'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-PN9GQLW7GC`}
+      />
+
+      <Script strategy="lazyOnload">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-PN9GQLW7GC');
+        `}
+      </Script>
+
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp
